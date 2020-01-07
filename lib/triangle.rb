9709866a9ee268d038ff8ebc3c37ffb4faeffa2c
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-01-07 03:50:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,17 @@
-class Triangle
-  # write code here
+class Triangle 
+  attr_accessor :side_1, :side_2, :side_3 
+  
+  def initialize(attributes)
+
+    def kind
+    if @s1 == @s2 && @s1 == @s3
+      return :equilateral
+    elsif @s1 == @s2 || @s2 == @s3 || @s3 == @s1
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
 end
+  
